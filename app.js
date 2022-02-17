abaf2427df35d250f2914ec1f284a24c65c8ff2a
raselcsedev <raselcse.dev@gmail.com>
2022-02-17 19:25:00 +0600

    update balance and expence

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
+function errorMessasge(){
+    document.getElementById('calculate-btn').addEventListener('click', function(){
+        const incomeField = document.getElementById('income-field').value;
+        const failedMessage = document.getElementById('notify-failed');
+        if(isNaN(incomeField)==true){
+            failedMessage.style.display='block';
+
+        }
+        
+
+    })
+}
+errorMessasge();
+
 function getInput(amount, total, isAdd){
     const inputField = document.getElementById(amount +'-field');
     const inputText= inputField.value;
     const inputValue = parseFloat(inputText);
+    if(inputValue>0){ 
     const totalBalance = document.getElementById('total-'+ total);
     const currentBalance = parseFloat(totalBalance.innerText);
     if(isAdd == true){
         const newBalance = currentBalance + inputValue;
     totalBalance.innerText = newBalance;
-    // inputField.value = '';
+    inputField.value = '';
 
     }
     else{
@@ -15,38 +30,22 @@ function getInput(amount, total, isAdd){
         totalBalance.innerText = newBalance;
     // inputField.value = '';
     }
+    }
     
 
 }
 
 document.getElementById('calculate-btn').addEventListener('click', function(){
     getInput('income', 'balance', true);
-    
-    
-    
-})
-document.getElementById('calculate-btn').addEventListener('click', function(){
-    getInput('food', 'balance', false)
-    
-    
-})
-document.getElementById('calculate-btn').addEventListener('click', function(){
-    getInput('rent', 'balance', false)
-    
-    
-})
-document.getElementById('calculate-btn').addEventListener('click', function(){
-    getInput('cloth', 'balance', false)
-    
-    
-})
-document.getElementById('calculate-btn').addEventListener('click', function(){
-    getInput('food', 'expence', true)   
-})
-document.getElementById('calculate-btn').addEventListener('click', function(){
-    getInput('rent', 'expence', true)   
-})
-document.getElementById('calculate-btn').addEventListener('click', function(){
-    getInput('cloth', 'expence', true)   
-})
+    getInput('food', 'balance', false);
+    getInput('rent', 'balance', false);
+    getInput('cloth', 'balance', false);
+    getInput('food', 'expence', true);
+    getInput('rent', 'expence', true);
+    getInput('cloth', 'expence', true);
 
+
+
+    
+    
+})
